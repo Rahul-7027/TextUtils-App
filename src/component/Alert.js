f44c4fc70ac83a7be96934b18eb08cbb2440d53b
Rahul-7027 <RAHULBHATI0202@GMAIL.COM>
2023-01-09 20:35:44 +0530

    First Commit

---
+++ src/component/Alert.js
@@ -0,0 +1,18 @@
+import React from 'react'
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+const Alert = (props) => {
+    const styleCss={
+        height:"50px"
+    }
+    return (
+        <div style={styleCss}>
+            {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+                <strong>{capitalizeFirstLetter(props.alert.type)}</strong> : {props.alert.msg}
+            </div>}
+        </div>
+    )
+}
+
+export default Alert
